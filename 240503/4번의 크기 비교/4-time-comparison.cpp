d5306524375401cd4d08e2527a78f891f6814761
wychoi <soc09072@gmail.com>
2024-05-03 22:28:58 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240503/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e;
+    std::cin >> a >> b >> c >> d >> e;
+    std::cout << a > b? 1:0 << std::endl;
+    std::cout << a > c? 1:0 << std::endl;
+    std::cout << a > d? 1:0 << std::endl;
+    std::cout << a > e? 1:0 << std::endl;
+    
+    return 0;
+}
