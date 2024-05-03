71867c2d8b4fa6d4dd64650403397b87921c26b5
wychoi <soc09072@gmail.com>
2024-05-03 22:30:59 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240503/4번의 크기 비교/4-time-comparison.cpp
+++ 240503/4번의 크기 비교/4-time-comparison.cpp
@@ -4,10 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c, d, e;
     std::cin >> a >> b >> c >> d >> e;
-    std::cout << a > b? 1:0 << std::endl;
-    std::cout << a > c? 1:0 << std::endl;
-    std::cout << a > d? 1:0 << std::endl;
-    std::cout << a > e? 1:0 << std::endl;
+    std::cout <<(( a > b)? 1:0) << std::endl;
+    std::cout << ((a > c)? 1:0 )<< std::endl;
+    std::cout << ((a > d)? 1:0) << std::endl;
+    std::cout << ((a > e)? 1:0) << std::endl;
     
     return 0;
 }
